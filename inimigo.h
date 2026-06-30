452baf77a24fed17323418d6ae7486c3c46b12fa
Aluno SENAI <Aluno@SN-405089.edu.fiesc.com.br>
2026-06-30 15:38:00 -0300

    Commit 2: Adiciona inimigo.h com funcao para exibir dados do inimigo

---
+++ inimigo.h
@@ -0,0 +1,10 @@
+#ifndef INIMIGO_H
+#define INIMIGO_H
+#include <iostream>
+#include <string>
+
+inline void exibirInimigo(std::string nome, int vida, int ataque) {
+    std::cout << "=== INIMIGO ===" << std::endl;
+    std::cout << "Nome: " << nome << " | Vida: " << vida << " | Ataque: " << ataque << std::endl;
+}
+#endif
