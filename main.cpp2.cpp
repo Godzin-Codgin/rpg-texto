b51886dd45c0903ff9d4a39f34e6314653fd33d2
Aluno SENAI <Aluno@SN-405089.edu.fiesc.com.br>
2026-06-30 15:39:32 -0300

    Commit 3: Cria main.cpp utilizando as funcoes de heroi e inimigo

---
+++ main.cpp2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "heroi.h"
+#include "inimigo.h"
+
+using namespace std;
+
+int main() {
+    exibirHeroi("Godzin prozin", 100, 25);
+    exibirInimigo("jooji matadorzin", 80, 45);
+
+    system("pause");
+    return 0;
+}
